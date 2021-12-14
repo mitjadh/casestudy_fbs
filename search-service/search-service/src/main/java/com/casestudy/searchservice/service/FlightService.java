80b5a0c38c81a8df4426778e5ed44f044c23266c
mitjadh <88079193+mitjadh@users.noreply.github.com>
2021-12-14 17:55:36 +0530

    Add files via upload

---
+++ search-service/search-service/src/main/java/com/casestudy/searchservice/service/FlightService.java
@@ -0,0 +1,24 @@
+package com.casestudy.searchservice.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.casestudy.searchservice.exception.FlightAlreadyExistsException;
+import com.casestudy.searchservice.exception.FlightNotFoundException;
+import com.casestudy.searchservice.models.Flights;
+
+public interface FlightService {
+	
+	public List<Flights> getAllFlight() throws FlightNotFoundException;
+	
+	public List<Flights> getFlight(String from_city,String to_city,String date_depart) throws FlightNotFoundException;
+	
+	ResponseEntity<Flights> addFlight(Flights flights) throws FlightAlreadyExistsException;
+	
+	ResponseEntity<?> updateFlight(int flight_id, Flights flights);
+
+	ResponseEntity<?> deleteFlight(int flight_id) throws FlightNotFoundException;
+	
+	
+}
